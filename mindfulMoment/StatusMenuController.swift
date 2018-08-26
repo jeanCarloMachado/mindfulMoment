b09c58fe4042c5853a1e9bdd838297dc1aef2b64
jeanCarloMachado <contato@jeancarlomachado.com.br>
2018-08-26 18:48:23 +0200

    refactorign

--- mindfulMoment/StatusMenuController.swift
+++ mindfulMoment/StatusMenuController.swift
@@ -1,10 +1,26 @@
 import Cocoa
 
 class StatusMenuController: NSObject {
+    let databaseFile = ".database.txt"
+
     @IBOutlet var statusMenu: NSMenu!
 
     @IBAction func quitClick(_: NSMenuItem) {
         NSApplication.shared.terminate(self)
+    }
+
+    @IBAction func addClick(_: NSMenuItem) {
+        // if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+        //     let fileURL = dir.appendingPathComponent(databaseFile)
+
+        //     let outString = "Write this text to the file"
+        //     do {
+        //         try outString.write(to: fileURL, atomically: true, encoding: .utf8)
+        //     } catch {
+        //         print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
+        //     }
+
+        // }
     }
 
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
@@ -15,20 +31,23 @@ class StatusMenuController: NSObject {
         statusItem.image = icon
         statusItem.menu = statusMenu
 
-        let file = ".database.txt" //this is the file. we will write to and read from it
+        let content = getDatabaseContent()
+        let editMenuItem = NSMenuItem()
+        editMenuItem.title = content
+        statusMenu.addItem(editMenuItem)
+    }
 
-        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-            let fileURL = dir.appendingPathComponent(file)
-
-            do {
-                let text = try String(contentsOf: fileURL, encoding: .utf8)
-                let editMenuItem = NSMenuItem()
-                editMenuItem.title = text
-                statusMenu.addItem(editMenuItem)
-            } catch {
-                print("error:", error)
+    func getDatabaseContent() -> String {
+        var text = ""
+        do {
+            if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                let fileURL = dir.appendingPathComponent(databaseFile)
+                text = try String(contentsOf: fileURL, encoding: .utf8)
             }
+        } catch {
+            print("error:", error)
         }
 
+        return text
     }
 }
